e83262919532295e93abfeb3998776a1908b73e0
Diana Salas <salasd10418@gmail.com>
2021-10-17 21:15:39 -0400

    finished adding routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,22 +1,47 @@
 const router = require('express').Router();
 
 const {
+  getAllThoughts,
+  getThoughtById,
   addThought,
-  removeThought,
+  updateThought,
   addReaction,
+  removeThought,
+  removeOnlyThought,
   removeReaction
 } = require('../../controllers/thought-controller');
 
-// /api/thoughts/<userId>
-router.route('/:userId').post(addThought);
+// /api/thoughts
+router
+  .route('/')
+  .get(getAllThoughts);
 
-// /api/thoughts/<userId>/<thoughtId>
+// /api/thoughts/:userId
+router
+  .route('/:userId')
+  .post(addThought);
+
+// /api/thoughts/:userId/:thoughtId
 router
   .route('/:userId/:thoughtId')
-  .put(addReaction)
+  .get(getThoughtById)
+  .put(updateThought)
   .delete(removeThought);
 
+// Remove thought not associated with user
+// /api/thoughts/:thoughtId
+router
+.route('/:thoughtId')
+.delete(removeOnlyThought);
+
+// /api/thoughts/:userId/:thoughtId/reactions
+router
+  .route('/:userId/:thoughtId/reactions')
+  .put(addReaction);
+
 // /api/thoughts/<userId>/<thoughtId>/<reactionId>
-router.route('/:userId/:thoughtId/:reactionId').delete(removeReaction);
+router
+  .route('/:userId/:thoughtId/:reactionId')
+  .delete(removeReaction);
 
 module.exports = router;
