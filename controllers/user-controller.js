1a7686569e8579a457d43b3c995f5e2cedf1045f
Diana Salas <salasd10418@gmail.com>
2021-10-17 21:17:43 -0400

    finished adding routes / added delete associated thoughts functionality

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,4 @@
-const { User } = require('../models');
+const { User, Thought } = require('../models');
 
 const userController = {
     // Get all users
@@ -10,10 +10,14 @@ const userController = {
             // Omits the version key from results for thoughts
             select: '-__v'
         })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
         // Omits the version key from results for user
         .select('-__v')
         // -1 denotes descending order
-        .sort({ _id: -1})
+        // .sort({ _id: -1})
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
@@ -26,6 +30,10 @@ const userController = {
         User.findOne({ _id: params.id })
         .populate({
             path: 'thoughts',
+            select: '-__v'
+        })
+        .populate({
+            path: 'friends',
             select: '-__v'
         })
         .select('-__v')
@@ -57,11 +65,48 @@ const userController = {
         .catch(err => res.json(err));
     },
 
-    // Delete user
+    // Delete user and associated thoughts
     deleteUser({ params }, res) {
         User.findOneAndDelete({ _id: params.id })
-        .then(dbUserData => res.json(dbUserData))
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found! '});
+                return;
+            };
+            return Thought.deleteMany({ userId: params.id });
+        })
+        .then(data => res.json(data))
         .catch(err => res.json(err));
+    },
+
+    // Create friend
+    createFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $addToSet: { friends: params.friendId }},
+            { new: true, runValidators: true }
+        )
+        .select('-__v')
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'User not found!' });
+                return;
+            };
+            return res.json(dbUserData);
+        })
+        .catch(err => res.json(err))
+    },
+
+    // Delete friend
+    deleteFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId }},
+            { new: true}
+        )
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err))
     }
 };
 
