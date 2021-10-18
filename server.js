d11a0f283b7372037db9f4b00e543cc5af0ed65e
Diana Salas <salasd10418@gmail.com>
2021-10-17 21:07:21 -0400

    removed unnecessary code /  added routes

--- server.js
+++ server.js
@@ -8,13 +8,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialize-api', {
-//   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
 mongoose.set('debug', true);
 
-// app.use(require('./routes'));
+app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}!`));
